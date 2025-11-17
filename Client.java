426787ac1953589842573e5c4c3e554399146d0d
Alwyn2005286 <chang.wyn02@gmail.com>
2025-11-17 10:15:36 +0800

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,6 +1,9 @@
 public class Client {
     private String clientID;
     private String name;
+    private String address;
+    private String contactPerson;
+    private String contactInfo;
 
     public Client(String clientID, String name) {
         this.clientID = clientID;
