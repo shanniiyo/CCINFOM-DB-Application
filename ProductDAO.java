c98e026dd32e1ab091774d26fab261ee046a262d
shanniiyo <153742714+shanniiyo@users.noreply.github.com>
2025-11-18 15:25:23 +0800

    Update ProductDAO.java

--- ProductDAO.java
+++ ProductDAO.java
@@ -4,18 +4,18 @@ import java.time.LocalDate;
 public class ProductDAO {
 
     // Load record from DB
-    public static Product getProductByID(String productID) {
+    public static Product getProductByID(int productID) {
         String sql = "SELECT * FROM Product WHERE ProductID = ?";
 
         try (Connection conn = DatabaseConnector.getConnection();
              PreparedStatement stmt = conn.prepareStatement(sql)) {
 
-            stmt.setString(1, productID);
+            stmt.setInt(1, productID);
             ResultSet rs = stmt.executeQuery();
 
             if (rs.next()) {
                 return new Product(
-                    rs.getString("ProductID"),
+                    rs.getInt("ProductID"),
                     rs.getString("Brand"),
                     rs.getInt("Quantity"),
                     rs.getDouble("Price"),
@@ -32,14 +32,14 @@ public class ProductDAO {
     }
 
     // Update stock in DB
-    public static void updateProductQuantity(String productID, int newQty) {
+    public static void updateProductQuantity(int productID, int newQty) {
         String sql = "UPDATE Product SET Quantity = ? WHERE ProductID = ?";
 
         try (Connection conn = DatabaseConnector.getConnection();
              PreparedStatement stmt = conn.prepareStatement(sql)) {
 
             stmt.setInt(1, newQty);
-            stmt.setString(2, productID);
+            stmt.setInt(2, productID);
             stmt.executeUpdate();
 
         } catch (SQLException e) {
@@ -48,14 +48,14 @@ public class ProductDAO {
     }
 
     // Update price in DB
-    public static void updateProductPrice(String productID, double newPrice) {
+    public static void updateProductPrice(int productID, double newPrice) {
         String sql = "UPDATE Product SET Price = ? WHERE ProductID = ?";
 
         try (Connection conn = DatabaseConnector.getConnection();
              PreparedStatement stmt = conn.prepareStatement(sql)) {
 
             stmt.setDouble(1, newPrice);
-            stmt.setString(2, productID);
+            stmt.setInt(2, productID);
             stmt.executeUpdate();
 
         } catch (SQLException e) {
