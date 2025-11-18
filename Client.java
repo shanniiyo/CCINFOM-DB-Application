2d2bc910a4a52a5d109a9af16e19352634e95edf
Alwyn2005286 <chang.wyn02@gmail.com>
2025-11-18 08:28:04 +0800

    Update Client.java

--- Client.java
+++ Client.java
@@ -5,9 +5,13 @@ public class Client {
     private String contactPerson;
     private String contactInfo;
 
-    public Client(String clientID, String name) {
+    public Client(String clientID, String name, String address,
+                  String contactPerson, String contactInfo) {
         this.clientID = clientID;
         this.name = name;
+        this.address = address;
+        this.contactPerson = contactPerson;
+        this.contactInfo = contactInfo;
     }
 
     public viewOrders(){
