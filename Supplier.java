a1df48bf9dd289ba25e38a4bd1cafdbbbe2f39cf
shanniiyo <153742714+shanniiyo@users.noreply.github.com>
2025-11-19 06:29:51 +0800

    Update Supplier.java

--- Supplier.java
+++ Supplier.java
@@ -1,50 +1,13 @@
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
+public class Supplier {
+    private String supplierID;
+    private String name;
 
-public class SupplierDAO {
-
-    //Select supplier
-    public static Supplier getSupplierByID(int supplierID) {
-        String sql = "SELECT * FROM Supplier WHERE SupplierID = ?";
-        try (Connection conn = DatabaseConnector.getConnection();
-             PreparedStatement stmt = conn.prepareStatement(sql)) {
-
-            stmt.setInt(1, supplierID);
-            ResultSet rs = stmt.executeQuery();
-
-            if (rs.next()) {
-                return new Supplier(
-                        rs.getString("SupplierID"),
-                        rs.getString("Name")
-                );
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return null;
+    public Supplier(String supplierID, String name) {
+        this.supplierID = supplierID;
+        this.name = name;
     }
 
-    // all suppliers
-    public static List<Supplier> getAllSuppliers() {
-        List<Supplier> suppliers = new ArrayList<>();
-        String sql = "SELECT * FROM Supplier";
-
-        try (Connection conn = DatabaseConnector.getConnection();
-             Statement stmt = conn.createStatement();
-             ResultSet rs = stmt.executeQuery(sql)) {
-
-            while (rs.next()) {
-                suppliers.add(new Supplier(
-                        rs.getString("SupplierID"),
-                        rs.getString("Name")
-                ));
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return suppliers;
-    }
+    public String getSupplierID() { return supplierID; }
+    public String getName() { return name; }
 }
+
